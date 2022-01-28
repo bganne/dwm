7918deb7f29b17453ad1038f5aefdcbded7a23e2
Benoît Ganne <benoit.ganne@gmail.com>
2022-01-28 18:13:55 +0100

    add suspend support

--- config.mk
+++ config.mk
@@ -23,12 +23,15 @@ FREETYPEINC = /usr/include/freetype2
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
+LIBS += -lXRes
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+CFLAGS  += -Wno-unused-function
 LDFLAGS  = ${LIBS}
+#CFLAGS  += -g -O0
+#LDFLAGS += -g
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
